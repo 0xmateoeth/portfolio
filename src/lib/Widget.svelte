74a91cdb8ff25b268d470437c26298b29398edfe
Stanislawycz <mateoguybinance@gmail.com>
2025-03-31 19:48:13 +0300

    init

---
+++ src/lib/Widget.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    let { name, description, style, url, children } = $props();
+</script>
+
+<a href={url}>
+    <div class="w-48 h-48 rounded-2xl border border-neutral-500 shadow-sm {style} flex flex-col p-5 pb-2.5 transition hover:scale-102">
+        <div class="h-full">
+            {@render children?.()}
+        </div>
+        <div class="h-fit">
+            <p class="text-3xl font-bold">{name}</p>
+            <p class="text-sm">{description}</p>
+        </div>
+    </div>
+</a>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+    import Widget from "$lib/Widget.svelte";
+    import { SiDiscord, SiTelegram, SiX } from "@icons-pack/svelte-simple-icons";
+</script>
+
+<main class="min-w-screen inter min-h-screen text-white max-h-full p-5 flex justify-center pattern-boxes pattern-neutral-800 pattern-bg-neutral-900 pattern-size-4 pattern-opacity-100">
+    <div class="app w-full md:w-4/5 flex flex-col md:flex-row gap-2">
+        <div class="profile w-full h-[96vh] md:w-1/2 md:h-full p-5 pt-[12%] flex flex-col gap-4 bg-gray-800/30 rounded-4xl backdrop-blur-[2px] border border-gray-500">
+            <div class="profile-picture w-24 aspect-square rounded-full bg-white"><img alt="logo" src="https://pbs.twimg.com/profile_images/1891787638966771712/-1WC4kAO_x96.jpg" class="rounded-full"></div>
+            <div class="flex flex-row gap-5 flex-wrap">
+                <p class="text-6xl w-fit font-bold">0xmateo</p>
+                <div class="flex flex-1 flex-row gap-3 w-full items-center md:mt-3">
+                    <a href="https://x.com/ludohacker">
+                        <SiX size={32}/>
+                    </a>
+                    <a href="https://t.me/ludohacker5">
+                        <SiTelegram size={32}/>
+                    </a>
+                    <a href="https://discordapp.com/users/795590409619374090">
+                        <SiDiscord size={32}/>
+                    </a>
+                </div>
+            </div>
+            <p class="text-lg md:text-xl ml-1">Web3 Explorer | Community Builder | Diving into crypto & beyond</p>
+            <div class="roles mt-[12%] font-semibold text-lg md:text-xl ml-1">
+                <p class="text-lg text-gray-300 font-normal">My roles:</p>
+                <div class="flex flex-row items-center">
+                    •
+                    <img src="Injective Symbol - White.png" alt="Logo" width="48px">
+                    <p class="mt-1">Warrior of Injective</p>
+                </div>
+            </div>
+        </div>
+        <div class="widgets w-full h-[96vh] md:w-1/2 md:h-full bg-gray-800/30 rounded-4xl backdrop-blur-[2px] border border-gray-500 p-8 mx-auto grid max-w-full grid-cols-1 gap-4 md:auto-rows-[12rem] md:grid-cols-3">
+            <Widget name="Post" description="Post" style="bg-black" url="https://x.com/ludohacker/status/1897262865720996342"><SiX size={64}/></Widget>
+            <Widget name="Post" description="Post" style="bg-black" url=""><SiX size={64}/></Widget>
+            <Widget name="Post" description="Post" style="bg-black" url=""><SiX size={64}/></Widget>
+            <Widget name="Post" description="Post" style="bg-black" url=""><SiX size={64}/></Widget>
+            <Widget name="Post" description="Post" style="bg-black" url=""><SiX size={64}/></Widget>
+        </div>
+    </div>
+</main>
