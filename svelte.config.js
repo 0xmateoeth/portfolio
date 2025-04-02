68f678fd7cb05bea08ddd20523e7278e50c0d259
Velikiy Kirill <kuklix@ya.ru>
2025-04-02 22:21:33 +0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,14 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			out: 'build',
+			precompress: true,
+		}),
+	},
 };
 
 export default config;
